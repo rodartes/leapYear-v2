25b4092b1071bb6ef904c772f07e30be807771b5
rodartes <rodartes@oregonstate.edu>
2021-03-08 02:33:33 +0000

    wrote tests for different years, tests passed.

--- test_leap.py
+++ test_leap.py
@@ -1,8 +1,13 @@
 import leap
-import pytest
 
 def test_leapYear():
   assert leap.leapyear(400) == True
+  assert leap.leapyear(4) == True
+  assert leap.leapyear(2000) == True
+  assert leap.leapyear(2020) == True
 
 def test_notLeapYear():
   assert leap.leapyear(100) == False
+  assert leap.leapyear(2019) == False
+  assert leap.leapyear(2018) == False
+  assert leap.leapyear(2017) == False
